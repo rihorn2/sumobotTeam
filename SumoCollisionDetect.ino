620e3dca279daf544a61b7e20530bdf526ad3104
Brandon Horn <rihorn@microsoft.com>
2018-10-18 10:30:02 -0400

    final?

--- SumoCollisionDetect.ino
+++ SumoCollisionDetect.ino
@@ -43,6 +43,10 @@
 #define LED 13
 Pushbutton button(ZUMO_BUTTON); // pushbutton on pin 12
 
+// Accelerometer Settings
+#define RA_SIZE 3  // number of readings to include in running average of accelerometer readings
+#define XY_ACCELERATION_THRESHOLD 2400  // for detection of contact (~16000 = magnitude of acceleration due to gravity)
+
 // Reflectance Sensor Settings
 #define NUM_SENSORS 6
 unsigned int sensor_values[NUM_SENSORS];
@@ -71,6 +75,7 @@ enum ForwardSpeed { SearchSpeed, SustainedSpeed, FullSpeed };
 ForwardSpeed _forwardSpeed;  // current forward speed setting
 #define FULL_SPEED_DURATION_LIMIT     2500  // ms, after which we retreat to the side
 #define CENTERING_TIME_LIMIT          800
+#define MAX_SEARCH_TIME  4000
 
 // Our team's state 
 #define DISTANCE_VECTOR_LENGTH   10
@@ -78,6 +83,8 @@ enum RobotState { SpinAndDetect, CloseIn, Kill, Centering};
 RobotState _state;
 float distancesLong[DISTANCE_VECTOR_LENGTH];
 float distancesNear[DISTANCE_VECTOR_LENGTH];
+unsigned long last_turn_time;
+unsigned long contact_made_time;
 unsigned long full_speed_start_time;
 unsigned int distanceLongIndex;
 unsigned int distanceLongCount;
@@ -96,22 +103,75 @@ unsigned long start_centering_time;
 #define MIN_DELAY_AFTER_TURN          400  // ms = min delay before detecting contact event
 #define MIN_DELAY_BETWEEN_CONTACTS   1000  // ms = min delay between detecting new contact event
 
-SharpIR SharpIRLong(A3, 1080);
-SharpIR SharpIRShort(A2, 430);
+SharpIR SharpIRLong(A2, 1080);
+SharpIR SharpIRShort(A3, 430);
 
-// C++ is a sad language
-void retreat(char direction);
-void checkForCentered();
-void checkAndCloseIn();
-void checkForDetections();
-void checkOnKillState();
-bool checkForBorderAndCorrect();
-void waitForButtonAndCountDown(bool restarting);
+// RunningAverage class
+// based on RunningAverage library for Arduino
+// source:  https://playground.arduino.cc/Main/RunningAverage
+template <typename T>
+class RunningAverage
+{
+  public:
+    RunningAverage(void);
+    RunningAverage(int);
+    ~RunningAverage();
+    void clear();
+    void addValue(T);
+    T getAverage() const;
+    void fillValue(T, int);
+  protected:
+    int _size;
+    int _cnt;
+    int _idx;
+    T _sum;
+    T * _ar;
+    static T zero;
+};
 
+// Accelerometer Class -- extends the LSM303 Library to support reading and averaging the x-y acceleration
+//   vectors from the onboard LSM303DLHC accelerometer/magnetometer
+class Accelerometer : public LSM303
+{
+  typedef struct acc_data_xy
+  {
+    unsigned long timestamp;
+    int x;
+    int y;
+    float dir;
+  } acc_data_xy;
+
+  public:
+    Accelerometer() : ra_x(RA_SIZE), ra_y(RA_SIZE) {};
+    ~Accelerometer() {};
+    void enable(void);
+    void getLogHeader(void);
+    void readAcceleration(unsigned long timestamp);
+    float len_xy() const;
+    float dir_xy() const;
+    int x_avg(void) const;
+    int y_avg(void) const;
+    long ss_xy_avg(void) const;
+    float dir_xy_avg(void) const;
+  private:
+    acc_data_xy last;
+    RunningAverage<int> ra_x;
+    RunningAverage<int> ra_y;
+};
+
+Accelerometer lsm303;
+boolean in_contact;  // set when accelerometer detects contact with opposing robot
+
+// forward declaration
+void setForwardSpeed(ForwardSpeed speed);
 void setup()
 {
   // Initiate the Wire library and join the I2C bus as a master
   Wire.begin();
+
+  // Initiate LSM303
+  lsm303.init();
+  lsm303.enable();
 
 #ifdef LOG_SERIAL
   Serial.begin(9600);
@@ -154,6 +214,10 @@ void waitForButtonAndCountDown(bool restarting)
   buzzer.playFromProgramSpace(sound_effect);
   delay(1000);
 
+  // reset loop variables
+  in_contact = false;  // 1 if contact made; 0 if no contact or contact lost
+  contact_made_time = 0;
+  last_turn_time = millis();  // prevents false contact detection on initial acceleration
   _forwardSpeed = SearchSpeed;
   _state = SpinAndDetect;
   full_speed_start_time = 0;
@@ -181,6 +245,7 @@ void loop()
   }
 
   loop_start_time = millis();
+  lsm303.readAcceleration(loop_start_time);
   sensors.read(sensor_values);
 
   // All states check for border
@@ -202,6 +267,18 @@ void loop()
       checkForCentered();
       break;
   }
+
+/* I don't THINK we want this once we have the full state machine
+  else  // otherwise, go straight
+  {
+    if (check_for_contact() || SharpIRLong.distance() < 10) 
+    { 
+      on_contact_made();
+    }
+    int speed = getForwardSpeed();
+    motors.setSpeeds(speed, speed);
+  }
+  */
 }
 
 void checkForCentered()
@@ -214,6 +291,8 @@ void checkForCentered()
   }
   else
   {
+    motors.setSpeeds(-200, 200);
+    last_turn_time = millis();
     _state = SpinAndDetect;
   }
 }
@@ -252,6 +331,7 @@ void checkOnKillState()
   {
     retreat(LEFT);
     _state = SpinAndDetect;
+    last_turn_time = millis();
     return;
   }
   // Look into ensuring we are pushing them front-on later...
@@ -260,6 +340,11 @@ void checkOnKillState()
 
 void checkForDetections() 
 {
+  if (check_for_contact()) {
+    motors.setSpeeds(400, 400);
+    full_speed_start_time = millis();
+    _state = Kill;
+  }
   const unsigned int initialIndex = distanceLongIndex;
   const float distance = SharpIRLong.distance();
   distancesLong[distanceLongIndex] = distance;
@@ -273,25 +358,281 @@ void checkForDetections()
     distanceLongIndex = 0;
   }
 
-  if (distance < 35) 
+  if (distance < 30) 
   {
-    motors.setSpeeds(400, 400);
-    full_speed_start_time = millis();
-    _state = Kill;
+    // start asymetrically, to unturn half a turn.
+    motors.setSpeeds(300, 200);
+    _state = CloseIn;
   }
   else 
   {
-    motors.setSpeeds(-200, 200);
+    if (loop_start_time - last_turn_time > MAX_SEARCH_TIME)
+    {
+      motors.setSpeeds(400, 400);
+      full_speed_start_time = millis();
+      _state = Kill;
+    }
+    else 
+    {
+      motors.setSpeeds(-200, 200);
+    }
   }
 }
 
 void checkAndCloseIn() 
 {
-  motors.setSpeeds(300, 300);
+  const float distance = SharpIRLong.distance();
+  if (distance < 12 || check_for_contact()) {
+    motors.setSpeeds(400, 400);
+    full_speed_start_time = millis();
+    _state = Kill;
+  }
+  else if(distance > 40)
+  {
+    last_turn_time = millis();
+    _state = SpinAndDetect;
+  }
+  else {
+    motors.setSpeeds(250, 250);
+  }
 }
 
 void retreat(char direction)
 {
   motors.setSpeeds(0.25 * direction * FULL_SPEED, direction * FULL_SPEED);
   delay(300);
+}
+
+// execute turn
+// direction:  RIGHT or LEFT
+// randomize: to improve searching
+void turn(char direction, bool randomize)
+{
+#ifdef LOG_SERIAL
+  Serial.print("turning ...");
+  Serial.println();
+#endif
+
+  // assume contact lost
+  on_contact_lost();
+
+  static unsigned int duration_increment = TURN_DURATION / 4;
+
+  // motors.setSpeeds(0,0);
+  // delay(STOP_DURATION);
+  motors.setSpeeds(-REVERSE_SPEED, -REVERSE_SPEED);
+  delay(REVERSE_DURATION);
+  motors.setSpeeds(TURN_SPEED * direction, -TURN_SPEED * direction);
+  delay(randomize ? TURN_DURATION + (random(8) - 2) * duration_increment : TURN_DURATION);
+  int speed = getForwardSpeed();
+  motors.setSpeeds(speed, speed);
+  last_turn_time = millis();
+}
+
+void setForwardSpeed(ForwardSpeed speed)
+{
+  _forwardSpeed = speed;
+  if (speed == FullSpeed) full_speed_start_time = loop_start_time;
+}
+
+int getForwardSpeed()
+{
+  int speed;
+  switch (_forwardSpeed)
+  {
+    case FullSpeed:
+      speed = FULL_SPEED;
+      break;
+    case SustainedSpeed:
+      speed = SUSTAINED_SPEED;
+      break;
+    default:
+      speed = SEARCH_SPEED;
+      break;
+  }
+  return speed;
+}
+
+// check for contact, but ignore readings immediately after turning or losing contact
+bool check_for_contact()
+{
+  static long threshold_squared = (long) XY_ACCELERATION_THRESHOLD * (long) XY_ACCELERATION_THRESHOLD;
+  return (lsm303.ss_xy_avg() >  threshold_squared) && \
+    (loop_start_time - last_turn_time > MIN_DELAY_AFTER_TURN) && \
+    (loop_start_time - contact_made_time > MIN_DELAY_BETWEEN_CONTACTS);
+}
+
+// sound horn and accelerate on contact -- fight or flight
+void on_contact_made()
+{
+#ifdef LOG_SERIAL
+  Serial.print("contact made");
+  Serial.println();
+#endif
+  in_contact = true;
+  contact_made_time = loop_start_time;
+  setForwardSpeed(FullSpeed);
+  buzzer.playFromProgramSpace(sound_effect);
+}
+
+// reset forward speed
+void on_contact_lost()
+{
+#ifdef LOG_SERIAL
+  Serial.print("contact lost");
+  Serial.println();
+#endif
+  in_contact = false;
+  setForwardSpeed(SearchSpeed);
+}
+
+// class Accelerometer -- member function definitions
+
+// enable accelerometer only
+// to enable both accelerometer and magnetometer, call enableDefault() instead
+void Accelerometer::enable(void)
+{
+  // Enable Accelerometer
+  // 0x27 = 0b00100111
+  // Normal power mode, all axes enabled
+  writeAccReg(LSM303::CTRL_REG1_A, 0x27);
+
+  if (getDeviceType() == LSM303::device_DLHC)
+  writeAccReg(LSM303::CTRL_REG4_A, 0x08); // DLHC: enable high resolution mode
+}
+
+void Accelerometer::getLogHeader(void)
+{
+  Serial.print("millis    x      y     len     dir  | len_avg  dir_avg  |  avg_len");
+  Serial.println();
+}
+
+void Accelerometer::readAcceleration(unsigned long timestamp)
+{
+  readAcc();
+  if (a.x == last.x && a.y == last.y) return;
+
+  last.timestamp = timestamp;
+  last.x = a.x;
+  last.y = a.y;
+
+  ra_x.addValue(last.x);
+  ra_y.addValue(last.y);
+
+#ifdef LOG_SERIAL
+ Serial.print(last.timestamp);
+ Serial.print("  ");
+ Serial.print(last.x);
+ Serial.print("  ");
+ Serial.print(last.y);
+ Serial.print("  ");
+ Serial.print(len_xy());
+ Serial.print("  ");
+ Serial.print(dir_xy());
+ Serial.print("  |  ");
+ Serial.print(sqrt(static_cast<float>(ss_xy_avg())));
+ Serial.print("  ");
+ Serial.print(dir_xy_avg());
+ Serial.println();
+#endif
+}
+
+float Accelerometer::len_xy() const
+{
+  return sqrt(last.x*a.x + last.y*a.y);
+}
+
+float Accelerometer::dir_xy() const
+{
+  return atan2(last.x, last.y) * 180.0 / M_PI;
+}
+
+int Accelerometer::x_avg(void) const
+{
+  return ra_x.getAverage();
+}
+
+int Accelerometer::y_avg(void) const
+{
+  return ra_y.getAverage();
+}
+
+long Accelerometer::ss_xy_avg(void) const
+{
+  long x_avg_long = static_cast<long>(x_avg());
+  long y_avg_long = static_cast<long>(y_avg());
+  return x_avg_long*x_avg_long + y_avg_long*y_avg_long;
+}
+
+float Accelerometer::dir_xy_avg(void) const
+{
+  return atan2(static_cast<float>(x_avg()), static_cast<float>(y_avg())) * 180.0 / M_PI;
+}
+
+
+
+// RunningAverage class
+// based on RunningAverage library for Arduino
+// source:  https://playground.arduino.cc/Main/RunningAverage
+// author:  Rob.Tillart@gmail.com
+// Released to the public domain
+
+template <typename T>
+T RunningAverage<T>::zero = static_cast<T>(0);
+
+template <typename T>
+RunningAverage<T>::RunningAverage(int n)
+{
+  _size = n;
+  _ar = (T*) malloc(_size * sizeof(T));
+  clear();
+}
+
+template <typename T>
+RunningAverage<T>::~RunningAverage()
+{
+  free(_ar);
+}
+
+// resets all counters
+template <typename T>
+void RunningAverage<T>::clear()
+{
+  _cnt = 0;
+  _idx = 0;
+  _sum = zero;
+  for (int i = 0; i< _size; i++) _ar[i] = zero;  // needed to keep addValue simple
+}
+
+// adds a new value to the data-set
+template <typename T>
+void RunningAverage<T>::addValue(T f)
+{
+  _sum -= _ar[_idx];
+  _ar[_idx] = f;
+  _sum += _ar[_idx];
+  _idx++;
+  if (_idx == _size) _idx = 0;  // faster than %
+  if (_cnt < _size) _cnt++;
+}
+
+// returns the average of the data-set added so far
+template <typename T>
+T RunningAverage<T>::getAverage() const
+{
+  if (_cnt == 0) return zero; // NaN ?  math.h
+  return _sum / _cnt;
+}
+
+// fill the average with a value
+// the param number determines how often value is added (weight)
+// number should preferably be between 1 and size
+template <typename T>
+void RunningAverage<T>::fillValue(T value, int number)
+{
+  clear();
+  for (int i = 0; i < number; i++)
+  {
+    addValue(value);
+  }
 }
